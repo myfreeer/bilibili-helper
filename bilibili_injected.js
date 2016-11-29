05a4e49e0305b8a8dedb82eaf9d46e549b1ed54c
myfreeer <myfreeer@users.noreply.github.com>
2016-11-29 15:14:56 +0800

    [html5] better support for comment cache

--- bilibili_injected.js
+++ bilibili_injected.js
@@ -131,18 +131,6 @@
 		return strTemp + Source;
 	}
 
-	var parseXmlSafe = function parseXmlSafe(text) {
-	    "use strict";
-	    text = text.replace(/[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]/g, "");
-	    if (window.DOMParser) return new window.DOMParser().parseFromString(text, "text/xml");
-	    else if (ActiveXObject) {
-	        var activeXObject = new ActiveXObject("Microsoft.XMLDOM");
-	        activeXObject.async = false;
-	        activeXObject.loadXML(text);
-	        return activeXObject;
-	    } else throw new Error("parseXmlSafe: XML Parser Not Found.");
-	};
-
 	function parseSafe(text) {
 		return ('' + text).replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 	}
