c739e8f291d809da51691003502ce0ec87eb251a
myfreeer <myfreeer@users.noreply.github.com>
2017-03-18 16:15:21 +0800

    Update bilibiliForeignVideoProvider.js

--- dev/bilibiliForeignVideoProvider.js
+++ dev/bilibiliForeignVideoProvider.js
@@ -2,54 +2,46 @@
 var COMMONJS = typeof module == 'object' && module.exports;
 if (COMMONJS) {
   var md5 = require('./md5');
+  var sleep = require('./libs').sleep;
 }
-class bilibiliForeignVideoProvider {
-  constructor(cid, avid, page = 1) {
-    this.video = this.getVideo(cid, avid, page);
-  }
-  parseJsonforFlvjs(json) {
-    if (!json) return console.warn('parseJsonforFlvjs Failed: No JSON provided.');
-    var mediaDataSource = {};
-    mediaDataSource.type = 'flv';
-    if (parseInt(json.timelength)) mediaDataSource.duration = parseInt(json.timelength);
-    if (json.durl) mediaDataSource.segments = json.durl.map(obj => ({
-      "duration": obj.length,
-      "filesize": obj.size,
-      "url": obj.url.match("ws.acgvideo.com") ? obj.url : obj.url.replace(/^http:\/\//, "https://") //.replace("&dynamic=1", "")
-    }));
-    if (!json.durl) return console.warn('parseJsonforFlvjs Failed: Nothing to play.');
-    return mediaDataSource;
-  }
-  fetchretry(url, options) {
-    var retries = (options && options.retries) ? options.retries : 5;
-    var retryDelay = (options && options.retryDelay) ? options.retryDelay : 500;
-    return new Promise((resolve, reject) => {
-      let wrappedFetch = n => fetch(url, options).then(response => resolve(response)).catch(error => n > 0 ? setTimeout(() => wrappedFetch(--n), retryDelay) : resolve({
-        'code': -1,
-        'message': error
-      }));
-      wrappedFetch(retries);
-    });
-  }
-  getVideo(cid, avid, page) {
-    let urls = [
-      'https://www.biliplus.com/BPplayurl.php?cid=' + cid + '&otype=json&type=mp4',
-      'https://api.prprpr.me/dplayer/video/bilibili?cid=' + cid,
-      'https://www.biliplus.com/BPplayurl.php?cid=' + cid + '&otype=json&type=flv'
-    ];
-    let fetchArray = urls.map(url => this.fetchretry(url, {
-      credentials: 'include'
-    }).then(response => response.json()));
-    return Promise.all(fetchArray).then(array => this.processResult(array, this.parseJsonforFlvjs));
-  }
-  processResult(array, func) {
-    let mediaDataSource = func(array[2]);
-    if (array.length === 3) return {
-      lowResLink: array[0],
-      mp4Link: array[1],
-      flvLink: array[2],
-      mediaDataSource: mediaDataSource
+const bilibiliForeignVideoProvider = async(cid, avid, page = 1, credentials = 'include', retries = 5, retryDelay = 500) => {
+    let url = {};
+    url.low = 'https://www.biliplus.com/BPplayurl.php?cid=' + cid + '&otype=json&quality=1&type=mp4';
+    url.mp4 = 'https://www.biliplus.com/BPplayurl.php?cid=' + cid + '&otype=json&type=mp4';
+    url.flv = 'https://www.biliplus.com/BPplayurl.php?cid=' + cid + '&otype=json&type=flv';
+    const parseJsonforFlvjs = (json) => {
+        if (!json) return console.warn('parseJsonforFlvjs Failed: No JSON provided.');
+        var mediaDataSource = {};
+        mediaDataSource.type = 'flv';
+        if (parseInt(json.timelength)) mediaDataSource.duration = parseInt(json.timelength);
+        if (json.durl) mediaDataSource.segments = json.durl.map(obj => ({
+            "duration": obj.length,
+            "filesize": obj.size,
+            "url": obj.url.match("ws.acgvideo.com") ? obj.url : obj.url.replace(/^http:\/\//, "https://")
+        }));
+        if (!json.durl) return console.warn('parseJsonforFlvjs Failed: Nothing to play.');
+        return mediaDataSource;
     };
-  }
-}
+    const getVideoJson = async(url, retries) => {
+        let json;
+        try {
+            json = await fetch(url, {
+                credentials
+            }).then(response => response.json());
+        } catch (error) {
+            if (--retries > 0) {
+                await sleep(retryDelay);
+                return await getVideoJson(type, retries);
+            } else json = {
+                'code': -1,
+                'message': error
+            };
+        }
+        return json;
+    }
+    let video = {};
+    for (let i of ['low', 'mp4', 'flv']) video[i] = await getVideoJson(url[i]);
+    video.mediaDataSource = parseJsonforFlvjs(video.flv);
+    return video;
+};
 if (COMMONJS) module.exports = bilibiliForeignVideoProvider;
