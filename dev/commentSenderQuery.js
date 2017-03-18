3d928b9bfba6247ebef9592f2a9893e0015ad166
myfreeer <myfreeer@users.noreply.github.com>
2017-03-18 18:36:22 +0800

    Update commentSenderQuery.js

--- dev/commentSenderQuery.js
+++ dev/commentSenderQuery.js
@@ -1,17 +1,16 @@
 var COMMONJS = typeof module == 'object' && module.exports;
 if (COMMONJS) var { CRC32,  checkCRCHash } = require('./crc32');
-const commentSenderQuery = hash => {
-  'use strict';
-  return new Promise((resolve, reject) => {
-    if (sessionStorage['commentSenderProvider_hash_' + hash]) return resolve(JSON.parse(sessionStorage['commentSenderProvider_hash_' + hash]));
-    if (hash.indexOf('D') === 0) return resolve({});
+const commentSenderQuery = async(hash, retries = 5) => {
+    if (sessionStorage['commentSender_hash_' + hash]) return JSON.parse(sessionStorage['commentSender_hash_' + hash]);
+    if (hash.indexOf('D') === 0) return {};
     let mid = checkCRCHash(hash);
-    if (!mid) return resolve(mid);
-    fetch('http://api.bilibili.com/cardrich?mid=' + mid).then(res => res.json()).then(json => {
-      if (!json || !(json && json.data && json.data.card)) return console.warn('commentSenderProvider: api went wrong again.', resolve(false));
-      if (hash && (CRC32.bstr("" + mid) >>> 0) === parseInt(hash, 16)) sessionStorage['commentSenderProvider_hash_' + hash] = JSON.stringify(json.data.card);
-      resolve(json.data.card);
-    });
-  });
+    try {
+        let json = await fetch('http://api.bilibili.com/cardrich?mid=' + mid).then(res => res.json());
+        if (hash && (CRC32.bstr("" + mid) >>> 0) === parseInt(hash, 16)) sessionStorage['commentSender_hash_' + hash] = JSON.stringify(json.data.card);
+        return json.data.card;
+    } catch (e) {
+        if (--retries > 0) return await commentSenderQuery(hash, retries);
+        else return mid;
+    }
 };
 if (COMMONJS) module.exports = commentSenderQuery;
